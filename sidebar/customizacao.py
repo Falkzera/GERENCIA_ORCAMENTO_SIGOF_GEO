6f437f3641da0832950b221bfda7b4f5919108a4
Falkzera <falcovisk@gmail.com>
2025-05-07 08:54:07 -0300

    Adicionado novos usuário

--- sidebar/customizacao.py
+++ sidebar/customizacao.py
@@ -9,7 +9,11 @@ def customizar_sidebar():
     col1, col2 = st.columns([2,1.2])
     col1.title("Sistema de Gestão Orçamentária")
     col2.image("image/SEPLAG.png")
-    st.write('---')
+    st.write('---') 
+    if "username" in st.session_state:
+        st.sidebar.markdown(f"<div style='text-align: center; font-size: 18px; color: #095aa2;'>{st.session_state.username}</div>", unsafe_allow_html=True)
+    st.sidebar.markdown("<br>", unsafe_allow_html=True)
+
 
 
         
